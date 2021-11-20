198d183541f0aea2417ffea0aae3b297db3c23f0
Sayali Nate <79188070+Sanju0506@users.noreply.github.com>
2021-11-20 11:22:36 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main();
+{
+    int i;
+    char str[5][50], temp[50];
+
+    printf("Enter 5 words: ");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("str[i],sizeof(str[i]),stdin");
+    }
+    if(strcmp(str[i],str[j]>0)
+    {
+       strcpy(temp,str[i])
+       strcpy(temp,str[i])
+       strcpy(temp,str[i])
+    }
+    scanf("");
+    return 0;
+}
